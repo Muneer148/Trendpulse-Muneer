916185a58589dfedd5fcfb48e2492f4fd893cf7a
Muneer148 <muneeru506@gmail.com>
2026-08-29 22:41:01 +0530

    Adding the function with no return value

--- task1_data_collection.py
+++ task1_data_collection.py
@@ -1,48 +1,45 @@
-import requests
+def get_trending_coins():    
+    import requests
 
-url = "https://api.coingecko.com/api/v3/search/trending"
+    url = "https://api.coingecko.com/api/v3/search/trending"
+    response = requests.get(url)
 
-response = requests.get(url)
+    print(response.status_code)
+    print(response.text[:500])
 
-print(response.status_code)
-print(response.text[:500])
+    data = response.json()
+    print(type(data))     
+    print(data.keys())  
 
-data = response.json()
-print(type(data))                    
-print(data.keys())  
+    print(type(data['coins']))
+    print(len(data['coins']))
+    print(data['coins'][0])
+    coin = data["coins"][0]["item"]
 
-print(type(data['coins']))
-print(len(data['coins']))
-print(data['coins'][0])
-coin = data["coins"][0]["item"]
+    print(coin["name"])
+    print(coin["symbol"])
+    print(coin["market_cap_rank"])
+    print(coin["data"]["price"])
 
-print(coin["name"])
-print(coin["symbol"])
-print(coin["market_cap_rank"])
-print(coin["data"]["price"])
+    coins = []
 
-coins = []
-
-for item in data["coins"]:
-    coin = item["item"]
+    for item in data["coins"]:
+        coin = item["item"]
 
     coin_info = {
-        "name": coin["name"],
-        "symbol": coin["symbol"],
-        "market_cap_rank": coin["market_cap_rank"],
-        "price": coin["data"]["price"],
-        "price_change_24h": coin["data"]["price_change_percentage_24h"]["usd"],
-        "market_cap": coin["data"]["market_cap"],
-        "total_volume": coin["data"]["total_volume"],
-        "score": coin["score"]
+    "name": coin["name"],
+    "symbol": coin["symbol"],
+    "market_cap_rank": coin["market_cap_rank"],
+    "price": coin["data"]["price"],
+    "price_change_24h": coin["data"]["price_change_percentage_24h"]["usd"],
+    "market_cap": coin["data"]["market_cap"],
+    "total_volume": coin["data"]["total_volume"],
+    "score": coin["score"]
     }
 
     coins.append(coin_info)
 
-print(coins)
-print(len(coins))
+    print(coins)
+    print(len(coins))
 
-import pandas as pd
-
-df = pd.DataFrame(coins)
-print(df)
+    return coins
