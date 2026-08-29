9a90d5fd959dfff290fc391acf6c8b1d1299282f
Shaik Muneeruddin <muneer.techai@gmail.com>
2026-08-29 21:53:55 +0530

    Extract and print coin details from API response
    Added functionality to extract and print detailed coin information from the API response, including price and market cap.

--- task1_data_collection.py
+++ task1_data_collection.py
@@ -9,3 +9,35 @@ print(response.text[:500])
 
 data = response.json()
 print(type(data))                    
+print(data.keys())  
+
+print(type(data['coins']))
+print(len(data['coins']))
+print(data['coins'][0])
+coin = data["coins"][0]["item"]
+
+print(coin["name"])
+print(coin["symbol"])
+print(coin["market_cap_rank"])
+print(coin["data"]["price"])
+
+coins = []
+
+for item in data["coins"]:
+    coin = item["item"]
+
+    coin_info = {
+        "name": coin["name"],
+        "symbol": coin["symbol"],
+        "market_cap_rank": coin["market_cap_rank"],
+        "price": coin["data"]["price"],
+        "price_change_24h": coin["data"]["price_change_percentage_24h"]["usd"],
+        "market_cap": coin["data"]["market_cap"],
+        "total_volume": coin["data"]["total_volume"],
+        "score": coin["score"]
+    }
+
+    coins.append(coin_info)
+
+print(coins)
+print(len(coins))
