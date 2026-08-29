b3775ab079e2eb377fe9c8139bf89e16380bf19e
Shaik Muneeruddin <muneer.techai@gmail.com>
2026-08-29 21:02:47 +0530

    Add data collection from CoinGecko API

--- task1_data_collection.py
+++ task1_data_collection.py
@@ -1,1 +1,11 @@
+import requests
 
+url = "https://api.coingecko.com/api/v3/search/trending"
+
+response = requests.get(url)
+
+print(response.status_code)
+print(response.text[:500])
+
+data = response.json()
+print(type(data))                    
