d92920b9bea8812f0772216e9a518467c6667930
Muneer148 <muneeru506@gmail.com>
2026-08-30 10:22:28 +0530

    Built the data processing pipeline

--- task2_data_processing.py
+++ task2_data_processing.py
@@ -2,38 +2,23 @@ import pandas as pd
 from task1_data_collection import get_trending_coins
 coins = get_trending_coins()
 
-df = pd.DataFrame(coins)
-print(df)
+def process_data(coins):
+    df = pd.DataFrame(coins)
 
-df.shape
-df.columns
-df.info()
-df.head()
+    df["market_cap"] = pd.to_numeric(
+        df["market_cap"].str.replace("$", "").str.replace(",", "")
+    )
 
-df["market_cap"] = pd.to_numeric(
-    df["market_cap"].str.replace("$", "").str.replace(",", "")
-)
+    df["total_volume"] = pd.to_numeric(
+        df["total_volume"].str.replace("$", "").str.replace(",", "")
+    )
 
-df["total_volume"] = pd.to_numeric(
-    df["total_volume"].str.replace("$", "").str.replace(",", "")
-)
+    return df
 
-print(df.dtypes)
+if __name__ == '__main__':
+    coins = get_trending_coins()
+    df = process_data(coins)
 
-top_gainer = df.loc[df["price_change_24h"].idxmax()]
-top_market_cap = df.loc[df["market_cap"].idxmax()]
-
-print("🔥 TOP GAINER")
-print(f"Coin: {top_gainer['name']} ({top_gainer['symbol']})")
-print(f"24h Change: {top_gainer['price_change_24h']:.2f}%")
-
-print("\n💰 HIGHEST MARKET CAP")
-print(f"Coin: {top_market_cap['name']} ({top_market_cap['symbol']})")
-print(f"Market Cap: ${top_market_cap['market_cap']:,}")
-
-top_coins = df.sort_values(
-    "price_change_24h",
-    ascending=False
-)
-
-print(top_coins[["name", "symbol", "price_change_24h"]])
+    print(df)
+    print('\nData Types:')
+    print(df.dtypes)
--- task4_visualization.py
+++ task4_visualization.py
@@ -1,1 +1,16 @@
+import matplotlib.pyplot as plt
+import pandas as pd
 
+from task1_data_collection import get_trending_coins
+
+coins = get_trending_coins()
+df = pd.DataFrame(coins)
+df['market_cap'] = pd.to_numeric(df['market_cap'].str.replace('$', '').str.replace(',', ''))
+df['total_volume'] = pd.to_numeric(df['total_volume'].str.replace('$', '').str.replace(',', ''))
+df = df.sort_values('price_change_24h', ascending = False)
+
+plt.barh(df['name'],df['price_change_24h'])
+plt.xlabel('price_change_24h (%)')
+plt.ylabel('Coin Name')
+plt.title('TrendPulse - Trending Coin Price Changes')
+plt.show()
