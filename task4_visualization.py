151dd217c80f322add10a490d01d1e73133896a1
Muneer148 <muneeru506@gmail.com>
2026-08-30 11:09:28 +0530

    Improved the Visualization

--- task4_visualization.py
+++ task4_visualization.py
@@ -1,14 +1,49 @@
+import time
 import matplotlib.pyplot as plt
-import pandas as pd
 
 from task1_data_collection import get_trending_coins
+from task2_data_processing import process_data
 
-coins = get_trending_coins()
-df = pd.DataFrame(coins)
-df = df.sort_values('price_change_24h', ascending = False)
 
-plt.barh(df['name'],df['price_change_24h'])
-plt.xlabel('price_change_24h (%)')
-plt.ylabel('Coin Name')
-plt.title('TrendPulse - Trending Coin Price Changes')
-plt.show()
+# Create the figure and axes once
+fig, ax = plt.subplots(figsize=(10, 6))
+
+
+while plt.fignum_exists(fig.number):
+
+    coins = get_trending_coins()
+    df = process_data(coins)
+    df = df.sort_values("price_change_24h", ascending=False)
+
+    colors = [
+        "green" if value >= 0 else "red"
+        for value in df["price_change_24h"]
+    ]
+
+    ax.clear()
+
+    ax.barh(
+        df["name"],
+        df["price_change_24h"],
+        color=colors
+    )
+
+    ax.axvline(0)
+
+    for i, value in enumerate(df["price_change_24h"]):
+        ax.text(
+            value,
+            i,
+            f"{value:.1f}%",
+            va="center"
+        )
+
+    ax.set_xlabel("24h Price Change (%)")
+    ax.set_ylabel("Coin")
+    ax.set_title("TrendPulse — Live Trending Coin Price Changes")
+
+    ax.invert_yaxis()
+
+    plt.tight_layout()
+
+    plt.pause(60)
