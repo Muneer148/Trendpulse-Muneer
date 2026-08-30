57cf210fdd0c1241e67807d952e3eaf0140fa4d5
Muneer148 <muneeru506@gmail.com>
2026-08-30 15:07:38 +0530

    Task3 Analysis Integrated into Task4

--- task4_visualization.py
+++ task4_visualization.py
@@ -1,9 +1,9 @@
 from datetime import datetime
-import time
 import matplotlib.pyplot as plt
 
 from task1_data_collection import get_trending_coins
 from task2_data_processing import process_data
+from task3_analysis import analyze_data
 
 fig, ax = plt.subplots(figsize=(10, 6))
 while plt.fignum_exists(fig.number):
@@ -15,6 +15,15 @@ while plt.fignum_exists(fig.number):
         continue
     current_time = datetime.now().strftime('%H:%M:%S')
     df = process_data(coins)
+    if df is None:
+        print("No DataFrame to analyze.")
+        plt.pause(60)
+        continue
+
+    analysis = analyze_data(df)
+    top_gainer = analysis["top_gainer"]
+    bottom_loser = analysis["bottom_loser"]
+
     df = df.sort_values("price_change_24h", ascending=False)
 
     colors = [
@@ -42,8 +51,10 @@ while plt.fignum_exists(fig.number):
 
     ax.set_xlabel("24h Price Change (%)")
     ax.set_ylabel("Coin")
-    ax.set_title(f"TrendPulse — Live Trending Coin Price Changes"
-                 f" (Last Updated on: {current_time})  | Refreshing every 60 seconds")
+    ax.set_title(f"TrendPulse — Live Trending Coin Price Changes\n"
+                 f"Top: {top_gainer['name']} +{top_gainer['price_change_24h']:.1f}% | "
+                 f"Bottom: {bottom_loser['name']} {bottom_loser['price_change_24h']:.1f}%\n"
+                 f"(Last Updated on: {current_time})  | Refreshing every 60 seconds")
 
     ax.invert_yaxis()
     plt.tight_layout()
